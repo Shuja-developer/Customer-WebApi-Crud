39a9e0c02e8d2c64e317cfb46aaf4a897051eecb
Shuja-developer <khansajaullah793@gmail.com>
2024-08-05 15:52:12 +0500

    file

---
+++ MVCCrud_WebAPI`s/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCCrud_WebAPI_s.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
